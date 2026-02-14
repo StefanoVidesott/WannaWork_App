44d196bf215ac31a583d978f10b55f6f46bc9b73
Stefano Videsott <ste.videsott@gmail.com>
2026-02-14 19:23:43 +0100

    feat: allowing apiary to access the server

--- app/server.js
+++ app/server.js
@@ -37,6 +37,7 @@ app.use(express.json());
 const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://wannawork.docs.apiary.io/',
     process.env.FRONTEND_URL
 ].filter(Boolean);
 
